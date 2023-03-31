8bfe8d10c91f08f314d5b8c6636b244b5ec4e0b8
maneaniket2003 <114945029+maneaniket2003@users.noreply.github.com>
2023-03-31 23:46:37 +0530

    Add files via upload

---
+++ pointer_arithmetic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int A[] = {2,4,6,8,10};
+	int *p = A;
+	int *q = &A[3];
+	
+	cout<<p<<endl;
+	
+	p++;
+	
+	cout<<p<<endl;
+	
+	cout<<q-p;
+}
---
+++ pointer_to_object.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle {
+	public :
+	int length;
+	int breadth;
+	
+	int area() {
+		return length*breadth;
+	}
+	int perimeter() {
+		return 2*(length + breadth);
+	}
+};
+
+int main() {
+	Rectangle r;
+	Rectangle *p;
+	p = &r;
+	p->length = 10;
+	p->breadth = 5;
+	cout<<"Area is "<<p->area()<<endl;
+	cout<<"Perimeter is "<<p->perimeter()<<endl;
+}
